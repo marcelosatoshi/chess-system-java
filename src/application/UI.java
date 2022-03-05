ce4ea934e3ad5536b9a03675ef247fef74b4d73c
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-03-04 23:19:17 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -70,6 +70,10 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println("Turn :" + chessMatch.getTurn());
 		System.out.println("Waiting player:"  + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!!");
+					
+		}
 		
 	}
 	
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,24 +5,26 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
 
-	public ChessPiece(Board board,Color color) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		
+
 	}
 
 	public Color getColor() {
 		return color;
 	}
-	
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+
 	public boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
-	
 
 }
