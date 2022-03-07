c49cde7cd524196859c1eb1b1d24ec9eb669127a
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-03-07 03:15:27 -0300

    Special move castling

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -14,8 +14,8 @@ public class Pawn extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-
-		Position p = new Position(0, 0);
+		
+			Position p = new Position(0, 0);
 
 		if (getColor() == Color.WHITE) {
 			p.setValues(position.getRow() - 1, position.getColumn());
